f6b778672e4a96753709f7d6c75dbaefeb3d8f1a
GeorgeWang03 <657241044@qq.com>
2017-09-07 15:59:35 +0800

    Initialize Commit

---
+++ ContinuousIntegrationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContinuousIntegrationTest
+//
+//  Created by yingwang on 2017/9/7.
+//  Copyright © 2017年 com.cpeoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ContinuousIntegrationTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContinuousIntegrationTest
+//
+//  Created by yingwang on 2017/9/7.
+//  Copyright © 2017年 com.cpeoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
