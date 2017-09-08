b290434864ebec889410e4330cd642333e0990ad
GeorgeWang03 <657241044@qq.com>
2017-09-08 15:15:24 +0800

    fix(ViewController): Test for CI poll CVS

--- ContinuousIntegrationTest/ViewController.m
+++ ContinuousIntegrationTest/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // test for CI poll SVC
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 
